2c6733e52bcf3b740f5eb5afa843102509423b9e
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-04-05 21:01:00 +0530

    Create ReverseNumber.java

---
+++ ReverseNumber.java
@@ -0,0 +1,14 @@
+public class ReverseNumber
+{
+public static void main (String[]args)
+{
+int no = 1234;
+int rem = 0;
+while(no>0)
+{
+rem =(rem*10)+no%10;
+no=no/10;
+}
+System.out.println(rem);
+}
+}
