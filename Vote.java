120d7a8a694b01ab30fdbf1ebac2a0899195085d
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-04-05 21:47:07 +0530

    fileupdated

---
+++ Vote.java
@@ -0,0 +1,16 @@
+public class Vote
+{
+public static void main(String[]args)
+{
+String A,B, C ;
+ A = new String ("1234" ) ;
+ B = A ;
+ C = A + B ;
+ System.out.println(C);
+
+}
+}
+
+/* OUTPUT:
+c:\java>java Vote
+12341234*/
