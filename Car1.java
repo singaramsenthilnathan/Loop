6e12c8c49cfd788a32e66ec82f7608b3eaa08b06
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-04-05 21:45:54 +0530

    fileupdated

---
+++ Car1.java
@@ -0,0 +1,32 @@
+public class Car1
+{
+public static void main(String[]args)
+{
+/*char c = - 1 ;
+char c = '\u00FF' ;
+char c = (char) 4096 ;
+char c = 4096L ;
+char c = 'c' ;
+char c = "c" ;*/
+
+
+
+int a = - 1 ;
+char b = '\u00FF' ;
+char c = (char) 4096 ;
+long d = 4096L ;
+char e= 'c' ;
+String f = "c" ;
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+
+}
+
+
+
+}
