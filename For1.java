c5f24308dd8a4fbe7159b86b9479a15ef47dbf5a
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-04-07 09:01:41 +0530

    fileupdated

---
+++ For1.java
@@ -0,0 +1,16 @@
+public class For1
+{
+public static void main(String[]args)
+{
+int val = 1;
+for(int count=1;count<=4; count++)
+{
+for(int i=1;i<=3;i++)
+{
+System.out.print(val+" ");
+}
+val++;
+//System.out.println();
+}
+}
+}
