9c89088bfe5ddbf711e56ff033032f7f3363c0e0
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-04-07 08:31:26 +0530

    fileupdated

---
+++ DowhileloopDemo .java
@@ -0,0 +1,14 @@
+class DowhileloopDemo 
+{
+public static void main(String args[])
+{
+int x = 21, sum = 0;
+do 
+{
+sum=sum+x;
+x--;
+} 
+while (x >=10);
+System.out.println("Summation: " + sum);
+}
+}
