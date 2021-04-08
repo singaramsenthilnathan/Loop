e82ad4a759030bbc3a53641684b1cf88b25ea76f
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-04-08 09:30:13 +0530

    Create For9.java

---
+++ For9.java
@@ -0,0 +1,20 @@
+
+public class For9 {
+
+	public static void main(String[] args) 
+{
+		// TODO Auto-generated method stub
+for(int j=1;j<=5;j++)
+{
+for(int i=5;i>=j;i--)
+{
+System.out.print(" ");
+}
+for(int k=1;k<=j;k++)
+{
+	System.out.print("*"+ " ");
+}
+System.out.println();
+}
+}
+}
