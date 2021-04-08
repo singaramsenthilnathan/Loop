0e5a382dcae9fd1b6fed2ad3420da88f79030841
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-04-08 08:44:22 +0530

    Nested for Loop

---
+++ For3.java
@@ -0,0 +1,18 @@
+public class For3 
+{
+
+	public static void main(String[] args) 
+{
+		// TODO Auto-generated method stub
+for(int j=5;j>=1;j--)
+{
+	for(int i=1;i<=5;i++)
+{
+	System.out.print(j+" ");
+}
+	System.out.println();
+	System.out.println();
+	
+}
+}
+}
